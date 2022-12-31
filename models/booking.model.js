f6116e5bce6e0379f5db40495aaa0788eff841df
Jobayer_ak <jobayer.ak@gmail.com>
2022-12-31 22:31:13 +0600

    booking model is created and going to create booking route

--- models/booking.model.js
+++ models/booking.model.js
@@ -1,25 +1,50 @@
-const mongoos = require("mongoose");
+const mongoose = require("mongoose");
 const validator = require("validator");
+const ObjectId = mongoose.Types.ObjectId;
 
-const bookingSchema = mongoose.Schema({
-  treatmentName: {
-    type: String,
-    required: true,
-  },
-  patientName: {
-    type: String,
-    trim: true,
-    required: true,
+const bookingSchema = mongoose.Schema(
+  {
+    treatmentName: {
+      type: String,
+      required: true,
     },
-    email: {
-        type: String,
-        validate: [validator.isEmail, "Provide a valid Email!"],
-        trim: true,
-        lowercase: true,
-        unique: true,
-        required: [true, "Email address is required"],
+    treatmentId: [
+      {
+        type: ObjectId,
+        ref: "Slot",
       },
-});
+    ],
+    patientName: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    patientEmail: {
+      type: String,
+      validate: [validator.isEmail, "Provide a valid Email!"],
+      trim: true,
+      lowercase: true,
+      required: [true, "Email address is required"],
+    },
+    contactNumber: {
+      type: String,
+      validate: [
+        validator.isMobilePhone,
+        "Please provide a valid contact number!",
+      ],
+      required: true,
+    },
+    slot: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
 const Booking = mongoose.model("Booking", bookingSchema);
 
