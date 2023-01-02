3e3c31d94983b21576aa7b88f004e86c2c033c58
Jobayer_ak <jobayer.ak@gmail.com>
2023-01-02 20:06:38 +0600

    slot controller updated

--- controllers/slot.controller.js
+++ controllers/slot.controller.js
@@ -3,30 +3,9 @@ const Slot = require("../models/slots.model");
 const { getSlotsService } = require("../services/slots.service");
 const Booking = require("../models/booking.model");
 
-// exports.getSlots = async (req, res) => {
-//   try {
-//     const date = req.query.date;
-//     const slots = await getSlotsService();
-//     // console.log(req.cookies);
-
-//     res.status(200).json({
-//       status: "Success",
-//       data: slots,
-//     });
-//   } catch (error) {
-//     res.status(400).json({
-//       status: "Failed",
-//       message: "Couldn't find the time slots for doctors appointment!",
-//       error: error.message,
-//     });
-//   }
-// };
-
 exports.getSlots = async (req, res) => {
   try {
     const date = req.query.date;
-
-    const slots = await getSlotsService();
 
     const bookingSlots = await Slot.aggregate([
       {
