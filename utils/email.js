2118ffa6f8ffce54a8155ace6a40d10b33da438c
Jobayer_ak <jobayer.ak@gmail.com>
2023-02-20 20:41:38 +0600

    update send email

--- utils/email.js
+++ utils/email.js
@@ -1,25 +1,30 @@
 const nodemailer = require("nodemailer");
 
 // 1. Sending Email
-exports.sendMail = (mailInfo) => {
-  //Step 1: Creating the transporter
-  const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    auth: {
-      user: process.env.SENDER_EMAIL,
-      pass: process.env.SENDER_EMAIL_PASS,
-    },
-  });
+exports.sendMail = async (mailInfo) => {
+  try {
+    //Step 1: Creating the transporter
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      auth: {
+        user: process.env.SENDER_EMAIL,
+        pass: process.env.SENDER_EMAIL_PASS,
+      },
+    });
 
-  //Step 2: Setting up message options
-  const messageOptions = {
-    subject: mailInfo.subject,
-    html: mailInfo.html,
-    to: mailInfo.email,
-    from: "DOCTOR'S HUB",
-  };
+    //Step 2: Setting up message options
+    const messageOptions = {
+      subject: mailInfo.subject,
+      html: mailInfo.html,
+      to: mailInfo.email,
+      from: "DOCTOR'S HUB",
+    };
 
-  //Step 3: Sending email
-  transporter.sendMail(messageOptions);
+    //Step 3: Sending email
+    await transporter.sendMail(messageOptions);
+    console.log("email send");
+  } catch (error) {
+    console.log(error);
+  }
 };
