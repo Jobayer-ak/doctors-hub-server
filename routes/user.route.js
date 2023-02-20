40fa9f229ee89f8fb7e5e766d0f251f1183ecdde
Jobayer_ak <jobayer.ak@gmail.com>
2023-02-20 17:38:52 +0600

    package.json file is updated

--- routes/user.route.js
+++ routes/user.route.js
@@ -4,7 +4,7 @@ const userController = require("../controllers/user.controller");
 const verifyToken = require("../middlewares/verifyToken");
 const adminAuthorization = require("../middlewares/adminAuthorization");
 
-router.get("/admin/users", userController.getAllUsers);
+router.get("/admin/users", adminAuthorization, userController.getAllUsers);
 router.get("/logout", userController.logout);
 router.post("/signup", userController.signup);
 router.post("/login", userController.login);
